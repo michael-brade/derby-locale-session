8cd3c1f06953b3813b2fcab89566d0694ed1d941
psirenny <djtorres0@gmail.com>
2013-12-07 13:06:57 -0500

    split into two modules

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
-var _ = require('lodash')
-  , locale = require('locale');
-
 module.exports = function (app, options) {
   return {
     init: function () {
       return function (req, res, next) {
-        var model = req.getModel()
-          , defaultLocale = model.get('$lang.defaultLocale')
-          , prefferedLocales = (req.headers['accept-language'] || defaultLocale).split(',')
-          , supportedLocales = new locale.Locales(_.keys(model.get('$lang.translations')));
-
-        prefferedLocales.unshift(req.session.locale || '');
-        prefferedLocales = new locale.Locales(prefferedLocales);
-        req.session.locale = prefferedLocales.best(supportedLocales).toString();
+        var model = req.getModel();
+        model.push('$locale.preferred', req.session.locale);
         model.set('_session.locale', req.session.locale);
         next();
       };
     },
     routes: function () {
-      app.post('/language/changeLocale', function (req, res) {
-        var model = req.getModel();
-        var supportedLocales = _.keys(model.get('$lang.translations'));
-        if (!req.body.locale) return res.send(400, {error: 'locale missing'});
-        if (!_.contains(supportedLocales, req.body.locale)) return res.send(400, {error: 'unsupported locale'});
+      app.post('/locale/changeLocale', function (req, res, next) {
         req.session.locale = req.body.locale;
-        res.send();
+        next();
       });
 
       return function (req, res, next) {
