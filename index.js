cc6597857094add4d48b9d2440ac7da00485426b
psirenny <djtorres0@gmail.com>
2013-12-07 13:12:45 -0500

    refactor

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
 module.exports = function (app, options) {
   return {
-    init: function () {
-      return function (req, res, next) {
-        var model = req.getModel();
-        model.push('$locale.preferred', req.session.locale);
-        model.set('_session.locale', req.session.locale);
-        next();
-      };
-    },
-    routes: function () {
-      app.post('/locale/changeLocale', function (req, res, next) {
-        req.session.locale = req.body.locale;
-        next();
-      });
-
-      return function (req, res, next) {
-        next();
-      }
-    }
+    init: require('./init')(app, options),
+    routes: require('./routes')(app, options)
   };
 };
